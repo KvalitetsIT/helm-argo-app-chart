8f0806c46050d79d39721ad43eac36fad712b416
Morten Matthiesen <mmb@kvalitetsit.dk>
2024-05-31 13:25:31 +0200

    Update helm script

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -10,7 +10,7 @@ git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
 mkdir -p argoApps
 cp ../../argoApps-*.tgz argoApps
-helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
+helm repo index . --merge --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
 git add -A
 git commit -m "Adding new argoApps chart"
 git push https://$1@github.com/KvalitetsIT/helm-repo
